a50ec3e1784d8d7458536e94fc01f759bafcfe5c
goodbadboy <selfmanademola@gmail.com>
2024-11-05 02:29:44 +0100

    last commit

--- Data-structures/index.js
+++ Data-structures/index.js
@@ -103,36 +103,368 @@ arr.shift();
 // QUEUE IMPLEMENTATION
 // 1. peek() 2. isEmpty() 3. size() 4. print() 5. dequeue() 6. enqueue()
 
-class Queue {
-    constructor() {
-        this.items = []
-    }
+// class Queue {
+//     constructor() {
+//         this.items = []
+//     }
 
-    enqueue(element) {
-        this.items.push(element);
-    }
+//     enqueue(element) {
+//         this.items.push(element);
+//     }
 
-    dequeue() {
-        return this.items.shift()
-    }
+//     dequeue() {
+//         return this.items.shift()
+//     }
 
-    isEmpty() {
-        return this.items.length === 0
-    }
+//     isEmpty() {
+//         return this.items.length === 0
+//     }
 
-    peek() {
-        if(!this.isEmpty()) {
-            return this.items[0]
-        }
-        return null;
-    }
-    size() {
-        return this.items.length
-    }
-    print() {
-        console.log(this.items.toString());
+//     peek() {
+//         if(!this.isEmpty()) {
+//             return this.items[0]
+//         }
+//         return null;
+//     }
+//     size() {
+//         return this.items.length
+//     }
+//     print() {
+//         console.log(this.items.toString());
+//     }
+// }
+
+// const queue = new Queue()
+// console.log(queue.isEmpty());
+// queue.enqueue(10);
+// queue.enqueue(20);
+// queue.enqueue(30);
+// console.log(queue.dequeue())
+// queue.print();
+// console.log(queue.peek())
+
+
+
+// Optimized implemetation of queue
+// class Queue {
+//     constructor() {
+//         this.items = {}
+//         this.rear = 0;
+//         this.front = 0;
+//     }
+//     enqueue(element) {
+//         this.items[this.rear] = element;
+//         this.rear++;
+//     }
+        
+//     dequeue() {
+//         const item = this.items[this.front]
+//         delete this.items[this.front]
+//         this.front++
+//         return item
+//     }
+//     isEmpty() {
+//         return this.rear - this.front === 0
+//         }
+        
+//         peek() {
+//             return this.items[this.front]
+//         }
+//         size() {
+//              return this.rear - this.front
+//          }
+//          print() {
+//             console.log(this.items);
+//          }
+// }
+
+// const queue = new Queue()
+// console.log(queue.isEmpty());
+// queue.enqueue(10);
+// queue.enqueue(20);
+// queue.enqueue(30);
+// console.log(queue.dequeue())
+// queue.print();
+// console.log(queue.peek())
+// their time complexity is linear
+
+
+
+
+// CIRCULAR QUEUE
+// it also follows the fifo principle. it suppports enqueue and dequeue;
+// it is used in clock, streaming data, traffic lights implentation
+// enqueue, dequeue, isFull, isEmpty, peek, size, print
+// class Queue {
+//     constructor(capacity) {
+//         this.items = new Array(capacity)
+//         this.capacity = capacity
+//         this.currentLength = 0
+//         this.rear = -1
+//         this.front = -1
+//     } 
+
+//     isFull() {
+//         return this.currentLength === this.capacity
+//     }
+//     isEmpty() {
+//         return this.currentLength === 0
+//     }
+//     enqueue(element) {
+//         if(!this.isFull()) {
+//             this.rear = (this.rear + 1) % this.capacity
+//             this.items[this.rear] = element
+//             this.currentLength  += 1
+//             if(this.front === -1) {
+//                 this.front = this.rear
+//             }
+//         }
+//     }
+//     dequeue() {
+//         if(this.isEmpty()) {
+//             return null;
+//         }
+//         const item = this.items[this.front]
+//         this.items[this.front] = null
+//         this.front = (this.front + 1) % this.capacity
+//         this.currentLength -= 1
+//         if(this.isEmpty()) {
+//             this.front = -1
+//             this.rear = -1
+//         }
+//         return item;
+//     }
+//     peek() {
+//         if(!this.isEmpty()) {
+//            return this.items[0]
+//         }
+//         return null;
+//     }
+//     print() {
+//         if(this.isEmpty()) {
+//             console.log('queue is empty')
+//         } else {
+//             let i
+//             let str = ''
+//             for (let i = this.front; i != this.rear; i = (i + 1) % this.capacity) {
+//                 str += this.items[i] + ''
+                
+//             }
+//             str += this.items[i]
+//             console.log(str);
+//         }
+//     }
+// } 
+
+
+// const queue = new Queue(5)
+// console.log(queue.isEmpty())
+// queue.enqueue(10);
+// queue.enqueue(20);
+// queue.enqueue(30);
+// queue.enqueue(40);
+// console.log(queue.isFull())
+// queue.print()
+// console.log(queue.dequeue())
+// console.log(queue.peek())
+// queue.print();
+
+
+
+
+
+
+// LINKED LIST
+
+// a linked list is a linear data structure that included a series of connected node
+// each node consist of a data value and a pointer that points to the next node
+// the list elements can be easily inserted or removed without reallocation or reorganization of structure
+// random acess of elements is noit feasible
+// it supports: insertion, deletion, searching
+// applications: used in stacks and queue, image viewer
+// 1. Node class
+class Node {
+    constructor(value) {
+        this.value = value
+        this.next = null
     }
 }
 
-const queue = new Queue()
-console.log(queue.isEmpty());
+class LinkedList {
+    constructor() {
+        this.head = null
+        this.size = 0
+    }
+
+
+
+    isEmpty() {
+        return this.size === 0
+    }
+
+
+
+
+    getSize() {
+        return this.size
+    }
+
+
+
+
+    // it has a constant time complexity
+    prepend(value) {
+        const node = new Node(value)
+        if(this.isEmpty()) {
+            this.head = node
+        } else {
+            node.next = this.head
+            this.head = node
+        }
+        this.size++
+    }
+
+
+
+    // it has a linear time complexity
+    append(value) {
+        const node = new Node(value)
+        if(this.isEmpty()) {
+            this.head = node
+        } else {
+            let prev = this.head
+            while(prev.next) {
+                prev = prev.next
+            }
+            prev.next = node
+        } 
+        this.size++
+    }
+
+
+
+    insert(value, index) {
+        if(index < 0 || index > this.size) {
+            return
+        }
+        if (index === 0) {
+            this.prepend(value)
+        } else {
+            const node = new Node(value)
+            let prev = this.head
+            for (let i = 0; i < index - 1; i++) {
+                prev = prev.next
+            }
+            node.next = prev.next
+            prev.next = node
+            this.size++
+        }
+    }
+
+
+
+    removeFrom(index) {
+        if(index < 0 || index > this.size) {
+            return null
+        }
+        let removeNode;
+        if(index === 0){
+            removeNode = this.head
+            this.head = this.head.next
+        } else {
+            let prev = this.head
+            for (let i = 0; i < index - 1; i++) {
+                prev = prev.next
+            }
+            removeNode = prev.next
+            prev.next = removeNode.next
+        }
+        this.size--
+        return removeNode.value
+    }
+
+
+
+
+    removeValue(value) {
+        if(this.isEmpty()) {
+            return null
+        }
+        if (this.head.value === value) {
+            this.head = this.next
+            this.size--
+            return value
+        } else {
+            let prev = this.head
+            while(prev.next && prev.next.value !== value) {
+                prev = prev.next
+            }
+            if (prev.next) {
+                const removeNode = prev.next
+                prev.next = removeNode.next
+                this.size--
+                return value
+            }
+            return null
+        }
+    }
+
+
+
+
+    search(value) {
+        if(this.isEmpty()) {
+            return -1
+        }
+        let i = 0
+        let curr = this.head
+        while(curr) {
+            if(curr.value === value) {
+                return i
+            }
+            curr = curr.next
+            i++
+        }
+        return -1
+    }
+
+
+
+    reverse() {
+        let prev = null
+        let curr = this.head
+        while(curr) {
+            let next = curr.next
+            curr.next = prev
+            prev = curr
+            curr = next
+        }
+        this.head = prev
+    }
+
+
+
+    print() {
+        if(this.isEmpty()) {
+            console.log('The list is empty')
+        } else {
+            let curr = this.head
+            let listValues = ''
+            while(curr) {
+                listValues += `${curr.value}`
+                curr = curr.next
+            }
+            console.log(listValues)
+        }
+    }
+}
+
+const list = new LinkedList()
+console.log(list.isEmpty());
+console.log(list.getSize());
+list.print();
+list.append(10)
+list.print()
+list.append(20)
+list.append(30)
+list.print();
--- index.js
+++ index.js
@@ -133,32 +133,162 @@ function bubbleSort(arr) {
 
 // 4. Merge sort
 //  Divide the array into two and seperate until there is only one arrary
-function mergeSort(arr) {
-    if (arr.length < 2) {
-        return arr;
-    }
-    const middle = Math.floor(arr.length / 2)
-    const leftArr = arr.slice(0, middle)
-    const rightArr = arr.slice(middle)
-    return merge(mergeSort(leftArr), mergeSort(rightArr))
-}
-function merge(leftArr, rightArr) {
-    const sortedArr = []
-    while (leftArr.length && rightArr.length) {
-        if(leftArr[0] <= rightArr[0]) {
-            sortedArr.push(leftArr.shift())
-        } else {
-            sortedArr.push(rightArr.shift())
-        }
-    }
-    return [...sortedArr, ...leftArr, ...rightArr]
-}
-const arr = [8, 3, -3, 0, -2, -8, 4, 7]
-mergeSort(arr);
-console.log(mergeSort(arr));
+// function mergeSort(arr) {
+//     if (arr.length < 2) {
+//         return arr;
+//     }
+//     const middle = Math.floor(arr.length / 2)
+//     const leftArr = arr.slice(0, middle)
+//     const rightArr = arr.slice(middle)
+//     return merge(mergeSort(leftArr), mergeSort(rightArr))
+// }
+// function merge(leftArr, rightArr) {
+//     const sortedArr = []
+//     while (leftArr.length && rightArr.length) {
+//         if(leftArr[0] <= rightArr[0]) {
+//             sortedArr.push(leftArr.shift())
+//         } else {
+//             sortedArr.push(rightArr.shift())
+//         }
+//     }
+//     return [...sortedArr, ...leftArr, ...rightArr]
+// }
+// const arr = [8, 3, -3, 0, -2, -8, 4, 7]
+// mergeSort(arr);
+// console.log(mergeSort(arr));
 // Big O is logn of n
 
 
+// LEET CODE QUESTIONS
+// Given a string s, find the length of the longest substring without repeating characters
+// Using sliding window approach
+function lengthOfSubstrings(st) {
+    let chars = new Set();
+    let left = 0;
+    let longest = 0;
+    for (let right = 0; right < s.length; right++) {
+        while (chars.has(s[right])) {
+            chars.delete(s[left]);
+            left++
+        }
+        chars.add(s[right]);
+        longest = Math.max(longest, right - left + 1)
+        
+    }
+    return longest
+}
+const st = 'aadbef'
+// console.log(lengthOfSubstrings(st));
+
+
+// LEET CODE QUSTIONS 2
+// Give two sorted arrays nums 1 and nums 2 of size m and n respectively, return the median of the two sorted arrays
+function medianSortedArrays(nums1, nums2) {
+    const merged = [...nums1, ...nums2].sort((a,b) => a - b);
+    const totalLength = merged.length
+
+    if (totalLength % 2 === 1) {
+        return merged[Math.floor(totalLength / 2)]
+    } else {
+        return (merged[totalLength / 2 - 1] + merged[totalLength / 2]) / 2
+    }
+}
+const nums1 = [1, 2, 3, 4, 5, 7, 8]
+const nums2 = [ 1, 3, 4, 6, 7, 8, 10]
+// 1,2,3,4,4,5,5,6,8,10
+// console.log(medianSortedArrays(nums1, nums2))
+
+// LEETCODE 3.
+// Given a string s, return the longest palindromic substring in s
+// e.g s = 'babad' ans = 'bab'or 'aba'  s = 'cbbd' ans = 'bb'
+function palindromSubstring(s) {
+    let longest = ''
+    for (let i = 0; i < s.length; i++) {
+        // odd length palindrome
+        let palindrome = palindromic(s, i, i);
+         // babad
+        if (palindrome.length > longest.length) {
+            longest = palindrome
+        }
+        // even length palindrome
+        palindrome = palindromic(s, i, i + 1)
+        if (palindrome.length > longest.length) {
+            longest = palindrome
+        }
+        if (palindrome.length > s.length / 2) {
+            break;
+        }
+        
+    }
+    return longest;
+}
+function palindromic(s, left, right) {
+    // babad
+    while (left >= 0 && right < s.length && s[left] === s[right]) {
+        left--;
+        right++
+    }
+    return s.slice(left + 1, right)
+}
+
+const s = 'babad'
+// console.log(palindromSubstring(s));
+
+var longestPalindrome = function (s) {
+    let check = function (i, j) {
+        let left = i;
+        let right = j - 1;
+
+        while (left < right) {
+            if (s.charAt(left) !== s.charAt(right)) {
+                return false;
+            }
+
+            left++;
+            right--;
+        }
+
+        return true;
+    };
+
+    for (let length = s.length; length > 0; length--) {
+        for (let start = 0; start <= s.length - length; start++) {
+            if (check(start, start + length)) {
+                return s.substring(start, start + length);
+            }
+        }
+    }
+
+    return "";
+};
+
+
+
+function isPalindrome(s) {
+    const reversed = s.split("").reverse().join("");
+    return s === reversed
+}
+// console.log(isPalindrome('madam'))
+
+// LEETCODE 4.
+// Given signed 32 bit integer x, return x with its digits reversed.
+//  if reversing x causes the value to go outside the signed 32bit integer range thn return 0
+function reverse(x) {
+    let sign = x < 0 ? -1 : 1;
+    let reversed = 0;
+    x = Math.abs(x);
+    while( x > 0 ) {
+        reversed = reversed * 10 + x % 10;
+        x = Math.floor(x / 10);
+        reversed = sign * reversed
+    }
+    if ( reversed <= -Math.pow(2, 31) || reversed > Math.pow(2, 31) -1) {
+        return 0
+    }
+    return reversed
+}
+const x = 42654
+console.log(reverse(x));
 
 
 // Algorithm design techniques
